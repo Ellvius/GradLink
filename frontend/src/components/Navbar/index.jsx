75b2cdc52c9312ef592385f8c2b33bb829356425
NavaneethChandran2005 <jnavaneethchandran@gmail.com>
2025-03-29 22:38:29 +0530

    GradLink enn aaki name

--- frontend/src/components/Navbar/index.jsx
+++ frontend/src/components/Navbar/index.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center">
           <Link href="/" className="text-xl font-bold">
-            BrandName
+            GradLink
           </Link>
         </div>
         
